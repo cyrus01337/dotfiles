6790f4c627c2dc0c9f801561d0e202cf7e6fceb9
cyrus01337 <cyrus21337@gmail.com>
2023-01-03 08:09:23 +0000

    Typo

--- deps.sh
+++ deps.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-yarn add @ianvs/prettier-plugin-sort-imports @tailwindcss/typography eslint postcss postcss-flexbug-fixes postcss-preset-env prettier prettier-plugin-sort-json tailwindcss -D
+yarn add @ianvs/prettier-plugin-sort-imports @tailwindcss/typography eslint postcss postcss-flexbugs-fixes postcss-preset-env prettier prettier-plugin-sort-json tailwindcss -D
 rm -f deps.sh
