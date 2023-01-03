14bf4990413617bebc591a990bd8e56fe54269db
cyrus01337 <cyrus21337@gmail.com>
2023-01-03 08:10:18 +0000

    Only remove script on successful install

--- deps.sh
+++ deps.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-yarn add @ianvs/prettier-plugin-sort-imports @tailwindcss/typography eslint postcss postcss-flexbugs-fixes postcss-preset-env prettier prettier-plugin-sort-json tailwindcss -D
-rm -f deps.sh
+yarn add @ianvs/prettier-plugin-sort-imports @tailwindcss/typography eslint postcss postcss-flexbugs-fixes postcss-preset-env prettier prettier-plugin-sort-json tailwindcss -D && rm -f deps.sh
