268329e4d942e187b24f778cd0b6af542bde5d86
cyrus01337 <cyrus21337@gmail.com>
2023-02-26 11:16:23 +0000

    Package manager agnosticism

--- deps.sh
+++ deps.sh
@@ -1,2 +1,19 @@
 #!/usr/bin/env bash
-yarn add @ianvs/prettier-plugin-sort-imports @tailwindcss/typography eslint postcss postcss-flexbugs-fixes postcss-preset-env prettier prettier-plugin-sort-json tailwindcss -D && rm -f deps.sh
+deps="@ianvs/prettier-plugin-sort-imports @tailwindcss/typography eslint postcss postcss-flexbugs-fixes postcss-preset-env prettier prettier-plugin-sort-json tailwindcss"
+cmd="npm install"
+
+if command -v pnpm &> /dev/null; then
+    cmd="pnpm add"
+elif command -v yarn &> /dev/null; then
+    cmd="yarn add"
+fi
+
+installed=$(eval "$cmd $deps -D")
+
+if [[ $installed ]]; then
+    exit 0
+else
+    rm -f deps.sh
+
+    exit 1
+fi
